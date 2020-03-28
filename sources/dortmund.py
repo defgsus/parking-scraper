3d9569dc14fdc6d2f11c8f194c81778c2c74e296
bergi <s.berke@netzkolchose.de>
2020-03-28 20:20:21 +0100

    add Duesseldorf meta

--- sources/dortmund.py
+++ sources/dortmund.py
@@ -10,7 +10,7 @@ class ParkingDortmund(DataSource):
     source_id = "digistadt-dortmund-parken"
     web_url = "https://geoweb1.digistadtdo.de/OWSServiceProxy/client/parken.jsp"
     city_name = "Dortmund"
-    
+
     def download_snapshot_data(self):
         soup = self.get_html_soup(self.web_url)
 
--- sources/duesseldorf.py
+++ sources/duesseldorf.py
@@ -12,16 +12,19 @@ class ParkingDuesseldorf(DataSource):
 
     source_id = "vtmanager-duesseldorf-parken"
     web_url = "https://vtmanager.duesseldorf.de/info/?parkquartier#main"
+    city_name = "Düsseldorf"
 
-    def download_snapshot_data(self):
+    def download_meta_data(self):
         markup = self.get_url(
             "https://vtmanager.duesseldorf.de/geoserverwfs",
             method="POST",
             data='<wfs:GetFeature xmlns:wfs="http://www.opengis.net/wfs" service="WFS" version="1.1.0" xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><wfs:Query typeName="feature:Parkhaeuser" srsName="EPSG:900913" xmlns:feature="http://vtmanager.duesseldorf.de/"/></wfs:GetFeature>',
             encoding="utf-8"
         )
+        return self.xml_to_dict(markup)
 
-        data = self.xml_to_dict(markup)
+    def download_snapshot_data(self):
+        data = self.download_meta_data()
 
         parking_places = []
 
@@ -39,6 +42,7 @@ class ParkingDuesseldorf(DataSource):
                 "id": "-".join(place_name.split()[:2]),
                 "num_all": num_all,
                 "num_free": num_cur,
+                "status": entry.get("{http://vtmanager.duesseldorf.de/}status"),
             })
 
         return parking_places
@@ -53,3 +57,32 @@ class ParkingDuesseldorf(DataSource):
             })
 
         return ret_data
+
+    def transform_meta_data(self, data):
+        data = self.download_meta_data()
+
+        parking_places = []
+
+        for entry in data["{http://www.opengis.net/gml}featureMembers"]["{http://vtmanager.duesseldorf.de/}Parkhaeuser"]:
+            num_all = self.int_or_none(entry.get("{http://vtmanager.duesseldorf.de/}kurzparkermax"))
+
+            place_name = entry["{http://www.opengis.net/gml}name"]
+
+            coordinates = None
+            coords = entry.get("{http://vtmanager.duesseldorf.de/}the_geom")
+            if coords:
+                coords = coords["{http://www.opengis.net/gml}Point"]
+                if coords:
+                    coords = coords["{http://www.opengis.net/gml}pos"].split()
+                    # TODO: need to convert to lat/lon
+                    coordinates = [float(x) for x in coords]
+
+            parking_places.append({
+                "place_name": place_name,
+                "id": "-".join(place_name.split()[:2]),
+                "num_all": num_all,
+                "place_url": entry.get("{http://vtmanager.duesseldorf.de/}vti_url"),
+                "coordinates": coordinates,
+            })
+
+        return super().transform_meta_data(parking_places)
