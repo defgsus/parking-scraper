dcef01fed1b7315a2141baa4725d6ab7a741153c
bergi <s.berke@netzkolchose.de>
2020-03-30 00:07:35 +0200

    fix hanau readout

--- sources/hanau.py
+++ sources/hanau.py
@@ -1,6 +1,7 @@
 import re
 import bs4
 import json
+from copy import deepcopy
 
 from util import DataSource
 
@@ -18,16 +19,18 @@ class ParkingHanau(DataSource):
 
         for div in soup.find_all("div", {"class": "well"}):
             span = div.find("span", {"class": "badge"})
-            prog = div.find("div", {"class": "progress-bar-danger"})
+            prog = div.find("div", {"class": "progress-bar-success"})
             if span and prog:
                 num_all = self.int_or_none(span.text.split()[0])
-                num_free = self.int_or_none(prog.text)
+                num_free = self.int_or_none(prog.text.split()[-1])
                 place_name = div.find("b").text.strip()
                 place_name = re.sub(r"\(.*\)", "", place_name)
                 place_id = place_name[place_name.index("ID:")+3:]
                 place_name = place_name[:place_name.index("ID:")].strip()
 
                 parking_places.append({
+                    # mixed-up num_free / occupied before v2
+                    "v": 2,
                     "place_name": place_name,
                     "place_id": place_id,
                     "num_all": num_all,
@@ -36,4 +39,20 @@ class ParkingHanau(DataSource):
 
         return parking_places
 
+    def transform_snapshot_data(self, data):
+        return super().transform_snapshot_data(self._fix_data(data))
+
+    def transform_meta_data(self, data):
+        return super().transform_snapshot_data(self._fix_data(data))
+
+    def _fix_data(self, data):
+        data = deepcopy(data)
+        for place in data:
+            if place.get("v", 0) < 2:
+                num_free = place.get("num_current")
+                if "num_free" in place:
+                    num_free = place["num_free"]
+
+                place["num_free"] = place["num_all"] - num_free
+        return data
 
