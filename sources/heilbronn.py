c6dc7adc316fc032833c40f44f6a680b61121fd5
bergi <s.berke@netzkolchose.de>
2020-04-01 16:08:49 +0200

    fix Heilbronn scraper

--- sources/heilbronn.py
+++ sources/heilbronn.py
@@ -15,12 +15,32 @@ class ParkingHeilbronn(DataSource):
 
         parking_places = []
 
-        for a in soup.find_all("a"):
+        for div in soup.find_all("div", {"class": "carparkLocation"}):
+            if div.find("a"):
+                place_name = div.find("a").text.strip()
+            else:
+                place_name = div.text.strip()
+
+            num_free = div.parent.parent.next_sibling.text.split()[-1]
+
             parking_places.append({
-                "place_name": a.text,
-                "num_free": self.int_or_none(a.parent.parent.parent.parent.text.split()[-1]),
+                "place_name": place_name,
+                "num_free": self.int_or_none(num_free),
             })
 
         return parking_places
 
+    def transform_snapshot_data(self, data):
+        return super().transform_snapshot_data(self._fix_data(data))
 
+    def transform_meta_data(self, data):
+        return super().transform_meta_data(self._fix_data(data))
+
+    def _fix_data(self, data):
+        """Filter out a typo3 parking house !??"""
+        new_data = []
+        for place in data:
+            if "typo3" in place["place_name"]:
+                continue
+            new_data.append(place)
+        return new_data
