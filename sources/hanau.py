426e8efde303fdeaa86c68b36fcfa03f8db3c99d
bergi <s.berke@netzkolchose.de>
2020-10-26 21:06:23 +0100

    fix removed ID in Hanau parking website

--- sources/hanau.py
+++ sources/hanau.py
@@ -11,6 +11,22 @@ class ParkingHanau(DataSource):
     source_id = "hanau-neu-erleben-parken"
     web_url = "http://www.hanau-neu-erleben.de/reise/parken/072752/index.html"
     city_name = "Hanau"
+    
+    PLACE_NAME_TO_OLD_ID = {
+        "Tiefgarage Am Markt": 1,
+        "Parkhaus Am Forum": 2,
+        "Parkhaus Nürnberger Straße": 3,
+        "Parkhaus Kinopolis 2": 4,
+        "Parkhaus Klinikum": 5,
+        "Parkhaus Congress Park": 6,
+        "Tiefgarage Congress Park": 7,
+        "Tiefgarage Forum": 8,
+        "Parkhaus Kinopolis": 9,
+        "Parkhaus City Center": 10,
+        "Tiefgarage Klinikum Süd": 11,
+        "Parkhaus Gloria Palais": 13,
+        "Parkplatz Main-Kinzig-Halle": 14,
+    }
 
     def download_snapshot_data(self):
         soup = self.get_html_soup(self.web_url)
@@ -25,12 +41,13 @@ class ParkingHanau(DataSource):
                 num_free = self.int_or_none(prog.text.split()[-1])
                 place_name = div.find("b").text.strip()
                 place_name = re.sub(r"\(.*\)", "", place_name)
-                place_id = place_name[place_name.index("ID:")+3:]
-                place_name = place_name[:place_name.index("ID:")].strip()
+
+                place_id = self.PLACE_NAME_TO_OLD_ID.get(place_name, place_name)
 
                 parking_places.append({
-                    # mixed-up num_free / occupied before v2
-                    "v": 2,
+                    # v2: mixed-up num_free / occupied before v2
+                    # v3: id got removed at 2020-08
+                    "v": 3,
                     "place_name": place_name,
                     "place_id": place_id,
                     "num_all": num_all,
