8287ed747934dd5941ea0391ffa8bd4a8f2b40e1
bergi <s.berke@netzkolchose.de>
2021-04-07 00:02:19 +0200

    fix Bielefeld

--- sources/bielefeld.py
+++ sources/bielefeld.py
@@ -13,33 +13,31 @@ class ParkingBielefeld(DataSource):
     def download_snapshot_data(self):
         soup = self.get_html_soup(self.web_url)
 
+        num_all_re = re.compile(".* (\d+) Plätze.*")
+        num_free_re = re.compile(".* (\d+) frei.*")
+
         parking_places = []
-        
-        num_all_re = re.compile("(\d+) Plätze.*")
-        num_free_re = re.compile(".* (\d+) frei")
 
         for table in soup.find_all("table"):
-            for tr in table.find_all("tr", {"class": "bghblau"}):
-                row = [td for td in tr.find_all("td")]
-                next_row = [td.text for td in tr.next_sibling.find_all("td")]
+            for tr in table.find_all("tr"):
+                text = tr.text.strip().replace("\n", " ")
+                if text.startswith("Kapazität"):
+                    num_all = num_all_re.match(text)
+                    if num_all:
+                        num_all = self.int_or_none(num_all.groups()[0])
 
-                place_name = list(row[1].find("b").children)[0].strip()
-                if "(" in place_name:
-                    place_name = place_name[:place_name.index("(")]
+                    num_free = num_free_re.match(text)
+                    if num_free:
+                        num_free = self.int_or_none(num_free.groups()[0])
 
-                places_str = next_row[1]
-                num_all = num_all_re.match(places_str)
-                if num_all:
-                    num_all = self.int_or_none(num_all.groups()[0])
+                    # now climb out of table to get place name
+                    h3 = table.parent.find("h3")
+                    place_name = h3.text.strip()
 
-                num_free = num_free_re.match(places_str)
-                if num_free:
-                    num_free = self.int_or_none(num_free.groups()[0])
-
-                parking_places.append({
-                    "place_name": place_name,
-                    "num_all": num_all,
-                    "num_free": num_free,
-                })
+                    parking_places.append({
+                        "place_name": place_name,
+                        "num_all": num_all,
+                        "num_free": num_free,
+                    })
 
         return parking_places
