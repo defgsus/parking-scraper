06dbaf2aeb32e069e8db83807e6260cc3689cdb8
bergi <s.berke@netzkolchose.de>
2021-04-06 23:13:17 +0200

    change Lübeck scraper (though IDs do not match anymore with previous data)

--- sources/luebeck.py
+++ sources/luebeck.py
@@ -1,6 +1,5 @@
-import bs4
 import json
-import ast
+import re
 
 from util import DataSource
 
@@ -8,39 +7,60 @@ from util import DataSource
 class ParkingLuebeck(DataSource):
 
     source_id = "parken-luebeck"
-    web_url = "https://www.parken-luebeck.de/parkmoeglichkeiten.html"
+    web_url = "https://www.parken-luebeck.de/"
     city_name = "Lübeck"
 
+    RE_PLACE_NUM = re.compile(r"\s*([^\|]+) | .* (\d+)\s+frei\s+/\s+(\d+)")
+
     def download_snapshot_data(self):
-        text = self.get_url(self.web_url)
-
-        search_str = "var parkingData = JSON.parse('"
-        idx = text.index(search_str)
-        if idx <= 0:
-            raise AssertionError("json data not found")
-
-        text = text[idx+len(search_str):]
-        text = text[:text.index("');")]
-        text = ast.literal_eval('"'+text+'"')
-        full_data = json.loads(text)
+        soup = self.get_html_soup(self.web_url)
 
         parking_places = []
-        for space in full_data.values():
+
+        for elem in soup.find_all("div", {"class": "location-list--item"}):
+            #match = self.RE_PLACE_NUM.match(div1.text)
+            #if match:
+            #    print(match.groups())
+            #else:
+            #    print(div1.text)
+
+            div = elem.find("div", {"class": "location-list-inner"})
+            place_name = div.text.strip().split("|")[0].strip()
+
+            div = elem.find("div", {"class": "free-live-spots"})
+            try:
+                num_free = int(div.text)
+            except:
+                num_free = None
+
+            div = elem.find("div", {"class": "free-spots"})
+            try:
+                num_all = int(div.text.split("/")[-1])
+            except:
+                num_all = None
+
             parking_places.append({
-                "place_name": space["title"],
-                "id": space["id"],
-                "num_all": self.int_or_none(space["totalSpace"]),
-                "num_free": self.int_or_none(space["free"]),
-                "status": space["state"],
+                "place_name": place_name,
+                "num_free": num_free,
+                "num_all": num_all,
             })
 
         return parking_places
+
+        # TODO: how to match with previous JSON based data? especially the ID?
+        #    parking_places.append({
+        #        "place_name": space["title"],
+        #        "id": space["id"],
+        #        "num_all": self.int_or_none(space["totalSpace"]),
+        #        "num_free": self.int_or_none(space["free"]),
+        #        "status": space["state"],
+        #    })
 
     def transform_snapshot_data(self, data):
         ret_data = []
         for entry in data:
             ret_data.append({
-                "place_id": self.place_name_to_id(entry["id"]),
+                "place_id": self.place_name_to_id(entry.get("id") or entry["place_name"]),
                 "num_free": entry.get("num_current") or entry.get("num_free")
             })
 
